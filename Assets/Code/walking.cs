881d98d4a3586e1eafcdc4bdfa3778e8c0300766
Owen Sawler <owensawler4@gmail.com>
2024-02-22 14:23:00 -0500

    added movement animation

--- Assets/Code/walking.cs
+++ Assets/Code/walking.cs
@@ -7,11 +7,12 @@ public class walking : MonoBehaviour
 
     public Rigidbody2D playerRigidBody;
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,15 @@ public class walking : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+        
+        if(moveInput != Vector2.zero){
+            animator.SetFloat("x-input", moveInput.x);
+            animator.SetFloat("y-input", moveInput.y);
+            animator.SetBool("isWalking", true);
+        }else{
+            animator.SetBool("isWalking", false);
+        }
+        
         moveInput.Normalize();
 
         playerRigidBody.velocity = moveInput * 10;
