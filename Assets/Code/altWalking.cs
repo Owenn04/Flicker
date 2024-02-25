f224802772ae571eb60f758fa4f4d2a44824714a
Vlad Lisitsyn <50640699+VladimirLisitsyn4@users.noreply.github.com>
2024-02-25 12:29:04 -0500

    added game over screen

--- Assets/Code/altWalking.cs
+++ Assets/Code/altWalking.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class altWalking : MonoBehaviour
 {
@@ -14,11 +15,13 @@ public class altWalking : MonoBehaviour
     public GameObject HP_1;
     public GameObject HP_0;
     public SpriteRenderer sprite;
+    public GameObject gameOverMenu;
 
     Vector2 mousePos;
     // Update is called once per frame
 
     void Start() {
+        gameOverMenu.SetActive(false);
         HP_2.gameObject.SetActive(false);
         HP_3.gameObject.SetActive(true);
         HP_1.gameObject.SetActive(false);
@@ -63,6 +66,7 @@ public class altWalking : MonoBehaviour
                 HP_3.gameObject.SetActive(false);
                 HP_1.gameObject.SetActive(false);
                 HP_0.gameObject.SetActive(true);
+                gameOver();
             }
 
         }
@@ -72,5 +76,22 @@ public class altWalking : MonoBehaviour
         sprite.color = Color.red;
         yield return new WaitForSeconds(0.2f);
         sprite.color = Color.white;
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver() {
+        gameOverMenu.SetActive(true);
+        Invoke("destroyEnemies", 0.5f);
+        AudioSource gunSound = gameObject.transform.Find("FirePoint").GetComponent<AudioSource>(); 
+        gunSound.enabled = false;
+
+    }
+
+    public void destroyEnemies() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(GameObject enemy in enemies)
+            Destroy(enemy);
     }
 }
