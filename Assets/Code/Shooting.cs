191f0fc80ba720b6f526658ec878233f6628346e
Vlad Lisitsyn <50640699+VladimirLisitsyn4@users.noreply.github.com>
2024-02-24 19:59:58 -0500

    added ui functionality and ammo system

--- Assets/Code/Shooting.cs
+++ Assets/Code/Shooting.cs
@@ -1,40 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
 
     public GameObject flashlight;
+    public GameObject flashlight_UI;
     private bool flashOn = true;
     public GameObject UVflashlight;
-    private bool UVflashOn = false;
+    public GameObject UVflashlight_UI;
     public GameObject bulletPrefab;
-
     public float force = 20f;
+    public int ammoCount = 13;
+    public Text ammoText;
 
     // Update is called once per frame
 
     void Start() {
         flashlight.gameObject.SetActive(true);
         UVflashlight.gameObject.SetActive(false);
+        flashlight_UI.gameObject.SetActive(true);
+        UVflashlight_UI.gameObject.SetActive(false);
     }
 
     void Update()
     {
 
-        if (Input.GetButtonDown("Fire1")) {
-            Shoot();
+        if (Input.GetButtonDown("Fire1")) {   
+            if (ammoCount > 0) {
+                Shoot();
+            }
         }
         if (Input.GetKeyDown(KeyCode.F)) {
             if (!flashOn) {
                 flashlight.gameObject.SetActive(true);
+                flashlight_UI.gameObject.SetActive(true);
                 UVflashlight.gameObject.SetActive(false);
+                UVflashlight_UI.gameObject.SetActive(false);
                 flashOn = true;
             } else {
                 flashlight.gameObject.SetActive(false);
+                flashlight_UI.gameObject.SetActive(false);
                 UVflashlight.gameObject.SetActive(true);
+                UVflashlight_UI.gameObject.SetActive(true);
                 flashOn = false;
             }
         }
@@ -45,5 +56,8 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
         bulletBody.AddForce(firePoint.right * force, ForceMode2D.Impulse);
+        ammoCount -= 1;
+        ammoText.text = ammoCount.ToString();
     }
+
 }
