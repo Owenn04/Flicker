2af348531def543bfb1d87296cc5f84837c5afd8
Vlad Lisitsyn <50640699+VladimirLisitsyn4@users.noreply.github.com>
2024-03-31 15:49:47 -0400

    Finished layout of first level

--- Assets/Code/CameraFollow.cs
+++ Assets/Code/CameraFollow.cs
@@ -10,17 +10,17 @@ public class CameraFollow : MonoBehaviour
     public float damping;
 
     private Vector3 velocity = Vector3.zero;
-    public float tLimit = 17;
-    public float bLimit = -17;
-    public float lLimit = -17;
-    public float rLimit = 17; 
+    public float tLimit = 30;
+    public float bLimit = -30;
+    public float lLimit = -30;
+    public float rLimit = 48; 
 
     void FixedUpdate()
     {
         Vector3 movePos = target.position + offset;
         Vector3 toMove = Vector3.SmoothDamp(transform.position, movePos, ref velocity, damping);
-        if (toMove.y < tLimit && toMove.y > bLimit && toMove.x > lLimit && toMove.x < rLimit) {
-            transform.position = toMove;
-        }
+        //if (toMove.y < tLimit && toMove.y > bLimit && toMove.x > lLimit && toMove.x < rLimit) {
+        transform.position = toMove;
+        //}
     }
 }
