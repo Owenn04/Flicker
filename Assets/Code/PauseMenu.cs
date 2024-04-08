af072160d11ca5b3b2557882482b3353aa9582a3
Owen Sawler <owensawler4@gmail.com>
2024-04-07 20:15:46 -0400

    pause screen and moved note

---
+++ Assets/Code/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Resume normal time scale
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Stop time
+        isPaused = true;
+    }
+
+    public void Restart()
+    {
+        // Add logic for restarting the game
+    }
+
+    public void Quit()
+    {
+        // Add logic for quitting the game
+    }
+}
